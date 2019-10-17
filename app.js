694deacf9de82f9412415395cb5d915409bb54da
Julia Aasmaa <my_e_10@hotmail.com>
2019-10-17 11:56:11 +0300

    commit

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const http = new myHTTP;
 
-const users = http.get('https://jsonplaceholder.typicode.com/users')
+/*const users = http.get('https://jsonplaceholder.typicode.com/users')
 .then(data => console.log("DATA Received:", data))
+.catch(error => console.log(error));*/
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+const data = {
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'jdoe@gmail.com'
+}
+
+/*http.post(url, data)
+.then(data => console.log(data))
+.catch(error => console.log(error));*/
+
+http.put('https://jsonplaceholder.typicode.com/users/2', data) 
+.then(data => console.log(data))
 .catch(error => console.log(error));
--- http.js
+++ http.js
@@ -25,5 +25,20 @@ class myHTTP {
         });
     }
 
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(error => reject(error));
+        });
+    }
+
 
 }
